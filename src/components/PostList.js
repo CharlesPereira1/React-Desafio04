4183bde94d2be2afbe6aaa65d5831b4b7a738d72
Charles Pereira <charlesti.ptu@gmail.com>
2019-10-31 01:12:14 -0300

    Criaçao de components PostList

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,7 +1,44 @@
 import React, { Component } from 'React'
 
 class PostList extends Component {
+  state = {
+    posts: [
+      {
+        id: 1,
+        author: {
+          name: "Charles Pereira",
+          avatar: "http://url-da-imagem.com/imagem.jpg",
+        },
+        date: "31 Out 2019",
+        content: "Pessoal, alguém sabe se a Rocketseat está contratando?",
+        comments: [
+          {
+            id: 1,
+            author: {
+              name: "Diego Fernandes",
+              avatar: "http://url-da-imagem.com/imagem.jpg"
+            },
+            content: "Conteúdo do comentário"
+          }
+        ]
+      }//,
+      // {
+      //id: 2
+      //restante dos dados de um novo post
+      // }
+    ]
+  }
+
   render() {
+    const { posts } = this.state;
+
+    return (
+      <div className="postlist">
+        {posts.map(post => (
+          <PostItem key={post.id} {...post} />
+        ))}
+      </div>
+    );
   }
 }
 
