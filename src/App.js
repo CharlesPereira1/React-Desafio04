df969a6afa21c1d011e2cd080c9eb6d803881ae1
Charles Pereira <charlesti.ptu@gmail.com>
2019-10-31 09:39:20 -0300

    Correção da pasta public e .gitignore

--- src/App.js
+++ src/App.js
@@ -3,13 +3,12 @@ import React from 'react';
 //import './App.css';
 
 import Header from './components/Header';
-import PostList from './components/PostList';
+//import PostList from './components/PostList';
 
 function App() {
   return (
     <>
       <Header />
-      <PostList />
     </>
   )
 }
