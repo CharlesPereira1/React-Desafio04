a1db996d87e7392af6e253312743b87efdbd4260
Charles Pereira <charlesti.ptu@gmail.com>
2019-11-01 01:06:53 -0300

    Component PostList configuração

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'React'
+import React, { Component } from 'react'
+
+import PostItem from './PostItem';
 
 class PostList extends Component {
   state = {
@@ -6,8 +8,8 @@ class PostList extends Component {
       {
         id: 1,
         author: {
-          name: "Charles Pereira",
-          avatar: "http://url-da-imagem.com/imagem.jpg",
+          name: "Ragnar Lothbrok",
+          avatar: "https://i.pravatar.cc/150?img=3",
         },
         date: "31 Out 2019",
         content: "Pessoal, alguém sabe se a Rocketseat está contratando?",
@@ -16,18 +18,77 @@ class PostList extends Component {
             id: 1,
             author: {
               name: "Diego Fernandes",
-              avatar: "http://url-da-imagem.com/imagem.jpg"
+              avatar: "https://avatars2.githubusercontent.com/u/2254731?v=4"
             },
-            content: "Conteúdo do comentário"
+            date: "31 Out 2019",
+            content: "A Rocketseat está sempre em busca de novos membros para o time, e geralmente ficamos de olho em quem se destaca no Bootcamp, inclusive 80% do nosso time de devs é composto por alunos do Bootcamp. Além disso, se você tem vontade de ensinar gravando vídeos e criando posts, pode me chamar no Discord! (Sério, me chamem mesmo, esse comentário é real)"
           }
         ]
-      }//,
-      // {
-      //id: 2
-      //restante dos dados de um novo post
-      // }
+      },
+      {
+        id: 2,
+        author: {
+          name: "Neil Cook",
+          avatar: "https://i.pravatar.cc/150?img=2",
+        },
+        date: "29 Out 2019",
+        content: "Fala galera, beleza?\nEstou fazendo o Bootcamp GoStack e está sendo muito massa! Alguém mais aí fazendo? Comenta aí na publicação para trocarmos uma idéia",
+        comments: [
+          {
+            id: 1,
+            author: {
+              name: "Clara Lisboa",
+              avatar: "https://i.pravatar.cc/150?img=5"
+            },
+            date: "29 Out 2019",
+            content: "Também estou fazendo o Bootcamp e estou adorando! Estou no terceiro módulo sobre Node e já tenho minha API dos desafios construída!",
+          },
+          {
+            id: 2,
+            author: {
+              name: "Cézar Toledo",
+              avatar: "https://i.pravatar.cc/150?img=11"
+            },
+            date: "30 Out 2019",
+            content: "Que maaaaaassa! Estou pensando em me inscrever na próxima turma pra ver qual é desse Bootcamp GoStack, dizem que os devs saem de lá com super poderes",
+          }
+
+        ],
+      },
+      {
+        id: 3,
+        author: {
+          name: 'Gabriel Lisboa',
+          avatar: 'https://i.pravatar.cc/150?img=51'
+        },
+        date: '04 Jun 2019',
+        content:
+          'Fala galera, beleza?\nEstou fazendo o Bootcamp GoStack e está sendo muito massa! Alguém mais aí fazendo? Comenta aí na publicação para trocarmos uma idéia',
+        comments: [
+          {
+            id: 1,
+            author: {
+              name: 'Clara Lisboa',
+              avatar: 'https://i.pravatar.cc/150?img=5'
+            },
+            date: '04 Jun 2019',
+            content:
+              'Também estou fazendo o Bootcamp e estou adorando! Estou no terceiro módulo sobre Node e já tenho minha API dos desafios construída!'
+          },
+          {
+            id: 2,
+            author: {
+              name: 'Cézar Toledo',
+              avatar: 'https://i.pravatar.cc/150?img=11'
+            },
+            date: '04 Jun 2019',
+            content:
+              'Que maaaaaassa! Estou pensando em me inscrever na próxima turma pra ver qual é desse Bootcamp GoStack, dizem que os devs saem de lá com super poderes'
+          }
+        ]
+      }
     ]
-  }
+  };
 
   render() {
     const { posts } = this.state;
@@ -36,7 +97,7 @@ class PostList extends Component {
       <div className="postlist">
         {posts.map(post => (
           <PostItem key={post.id} {...post} />
-        ))}
+        ))};
       </div>
     );
   }
