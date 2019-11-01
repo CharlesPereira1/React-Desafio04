24b0e546de0e2bd190c278b3c6bcbcdae6f87ce8
Charles Pereira <charlesti.ptu@gmail.com>
2019-11-01 00:23:38 -0300

    inicio do CSS e configuração do component Postitem

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'React'
 
+import profile1 from '../assets/profile1.png';
+
 class Header extends Component {
   render() {
     return (
       <header>
-        <nav>
-          <img src="https://i.imgur.com/KDIDiSE.png" />
+        <div class="content">
+          <nav>
+            <img src="https://i.imgur.com/KDIDiSE.png" />
+          </nav>
           <div>
             <span>Meu perfil</span>
-            <i className="material-icons">account_cicle</i>
+            <img src={profile1} />
           </div>
-        </nav>
+        </div>
       </header>
     )
   }
